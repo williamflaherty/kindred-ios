c856b19bf0454a1f3d4eef760cb8a4116c34c06e
William <william.flaherty@gmail.com>
2015-07-11 13:59:20 -0500

    Initial Commit

---
+++ Veronica/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Veronica
+//
+//  Created by Dwayne Flaherty on 7/11/15.
+//  Copyright (c) 2015 Willie Flaherty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Veronica/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Veronica
+//
+//  Created by Dwayne Flaherty on 7/11/15.
+//  Copyright (c) 2015 Willie Flaherty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
